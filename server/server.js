1a221d8429bddffa3f4f173c497b9c77294fb6f3
Sayali Pawar <apna-github-wala-email@yahan.com>
2026-08-08 23:03:08 +0530

    Render PORT fix

--- server/server.js
+++ server/server.js
@@ -402,7 +402,7 @@ require("./crop")(app, ai, db);
 // ===============================
 setupAuth(app);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 VED AI Server Running on Port ${PORT}`);
 });
